68fa0da460e98cc4d668cd533aa41a03bdb3210b
Jeffrey <jeffrey.liu90@gmail.com>
2020-07-20 01:33:14 -0400

    remove Element leetcode question with splice and pointer method

---
+++ removeElement.js
@@ -0,0 +1,11 @@
+const removeElement = (nums, val) => {
+  if (!nums) return 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums;
+};
